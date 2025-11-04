8ced527c8f503d034e234211324b62d90c658f95
Bonezegei <jofelbatutay@gmail.com>
2025-11-04 12:50:45 +0800

    update

---
+++ src/Bonezegei_74HC595.h
@@ -0,0 +1,26 @@
+/*
+  Bonezegei 74HC595 library
+  Author: Bonezegei (Jofel Batutay)
+  Date : November 4 2025
+*/
+
+#include <Arduino.h>
+
+#ifndef _BONEZEGEI_74HC595_H_
+#define _BONEZEGEI_74HC595_H_
+
+class Bonezegei_74HC595 {
+public:
+  Bonezegei_74HC595(int dataPin, int clockPin, int latchPin, int numRegisters = 1);
+  void begin();
+  void shiftOut(uint8_t data);
+
+  void clear();
+private:
+  int _dataPin;
+  int _clockPin;
+  int _latchPin;
+  int _numRegisters;
+};
+
+#endif
